6e2eb3087867112f0cfbe42dd0d811b0c6345bd5
JoseloR-D <joselo-rd@hotmail.com>
2018-05-03 04:18:00 -0400

    Creating and Deleting Roles.

--- PeliculasRD/Controllers/RoleAdminController.cs
+++ PeliculasRD/Controllers/RoleAdminController.cs
@@ -3,12 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Identity;
+using PeliculasRD.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
 
 namespace PeliculasRD.Controllers
 {
+    [Authorize]
     public class RoleAdminController : Controller
     {
+        RoleManager<IdentityRole> roleManager;
 
-    }
+        public RoleAdminController(RoleManager<IdentityRole> roleMgr)
+        {
+            roleManager = roleMgr;
+        }
+
+        public ViewResult Index() => View(roleManager.Roles);
+
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                } else
+                {
+                    AddErrorsFromResult(result);
+                }
+
+            } 
+
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+
+            if(role != null)
+            {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                } else
+                {
+                    AddErrorsFromResult(result);
+                }
+            } else
+            {
+                ModelState.AddModelError("", "Role not Found");
+            }
+            return View("Index", roleManager.Roles);
+        }
+
+        public void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach(IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }  
 }
