9b026276452a29229496c2277410ab3f19ea20a2
JoseloR-D <joselo-rd@hotmail.com>
2018-05-04 11:08:46 -0400

    Creating the Movie Controller.

--- PeliculasRD/Controllers/MovieController.cs
+++ PeliculasRD/Controllers/MovieController.cs
@@ -5,9 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 using PeliculasRD.Models;
 using PeliculasRD.Services.Interfaces;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PeliculasRD.Controllers
 {
+    [Authorize(Roles = "Admins")]
     public class MovieController : Controller
     {
         IMovieRepository repository;
@@ -16,11 +18,91 @@ namespace PeliculasRD.Controllers
             repository = movieRepository;
         }
 
+        [AllowAnonymous]
         public ViewResult Movies()
         {
             return View(repository.Movies());
         }
 
+        [AllowAnonymous]
+        public async Task<IActionResult> Movie(int id)
+        {
+            Movie movie = await repository.Movie(id);
 
+            if (movie != null)
+                return View(movie);
+            else
+                return NotFound();
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                Movie model = await repository.Create(movie);
+
+                if(movie != null)
+                {
+                    return RedirectToAction("Movies");
+                } else
+                {
+                    return NotFound();
+                }
+            }
+            ModelState.AddModelError("", "Por favor llene los campos");
+            return View(movie);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Movie model = await repository.Movie(id);
+            if (model != null)
+            {
+                return View(model);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Movie model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(model != null)
+                {
+                    Movie movie = await repository.Edit(model);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound();
+                }
+            } else
+            {
+                ModelState.AddModelError("", "Por favor llene los campos");
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Movie model = await repository.Movie(id);
+            if(model != null)
+            {
+                Movie movie = await repository.Delete(model);
+                if (movie != null)
+                    return RedirectToAction("Index");
+                else
+                    return NotFound();
+            }
+            return NotFound();
+        }
     }
 }
